265735d1c522bc24002ff3f644a00ffccd1ddb84
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-01-06 03:00:09 +0530

    completed the UI

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,11 +3,10 @@ import React from 'react';
 const Square = ({ value, onClick, isWinningSquare }) => (
   <button
     type="button"
-    className="square"
     onClick={onClick}
-    style={{
-      fontWeight: isWinningSquare ? `bold` : `normal`,
-    }}
+    className={`square ${isWinningSquare ? 'winning' : ''} ${
+      value === 'X' ? 'text-green' : 'text-orange'
+    }`}
   >
     {value}
   </button>
--- src/components/history.js
+++ src/components/history.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const History = ({ history, moveTo, currentMove }) => (
-  <ul>
-    {history.map((_, move) => (
-      <li key={move}>
-        <button
-          style={{
-            fontWeight: move === currentMove ? `bold` : `normal`,
-          }}
-          type="button"
-          onClick={() => {
-            moveTo(move);
-          }}
-        >
-          {move === 0 ? 'Go to game start' : `Go to move #${move}`}
-        </button>
-      </li>
-    ))}
-  </ul>
+  <div className="history-wrapper">
+    <ul className="history">
+      {history.map((_, move) => (
+        <li key={move}>
+          <button
+            className={`btn-move ${move === currentMove ? 'active' : ''}`}
+            type="button"
+            onClick={() => {
+              moveTo(move);
+            }}
+          >
+            {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default History;
